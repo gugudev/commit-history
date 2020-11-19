f48b2e4672e3d40b67053ec64df4ba317cee85c7
Gustavo Fe <gugudeveloper@gmail.com>
2020-11-18 23:37:07 -0300

    Mapped repository wrapper to view fields

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 
   def index
+    @repository = HttpRepositoryProviderAdapters::GithubAdapter
+                    .new('gugudev/commit-history')
+                    .fetch_commits
   end
 
 end
