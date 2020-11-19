75be63df17993f5a5190251aee51cbb1d62e7ecb
Gustavo Fe <gugudeveloper@gmail.com>
2020-11-18 23:42:31 -0300

    Added en locale and adjusted time field of commit

--- app/business/wrappers/commit_wrapper.rb
+++ app/business/wrappers/commit_wrapper.rb
@@ -5,7 +5,8 @@ module Wrappers
     attr_reader :sha, :message, :commiter_name, :commiter_email, :time_commit
 
     def initialize(sha, message, commiter_name, commiter_email, time_commit)
-     @sha, @message, @commiter_name, @commiter_email, @time_commit  = sha, message, commiter_name, commiter_email, time_commit
+     @sha, @message, @commiter_name, @commiter_email = sha, message, commiter_name, commiter_email
+     @time_commit = Time.parse(time_commit)
     end
 
   end
